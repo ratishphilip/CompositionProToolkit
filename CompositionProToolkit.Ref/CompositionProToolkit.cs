2ff7fbf5ef35d01864e5527520bb7cab6334921d
Ratish Philip <ratishphilip@gmail.com>
2016-06-15 10:46:41 -0700

    updated to v0.1.1
    Added Compositor Extension method to create Masked Backdrop Brush.

--- CompositionProToolkit.Ref/CompositionProToolkit.cs
+++ CompositionProToolkit.Ref/CompositionProToolkit.cs
@@ -4,6 +4,10 @@ namespace CompositionProToolkit
     {
         public static CompositionProToolkit.ICompositionMaskGenerator GetCompositionMaskGenerator(Windows.UI.Composition.Compositor compositor, Windows.UI.Composition.CompositionGraphicsDevice graphicsDevice=null, object sharedLock=null) { return default(CompositionProToolkit.ICompositionMaskGenerator); }
     }
+    public static partial class CompositorExtensions
+    {
+        public static Windows.UI.Composition.CompositionEffectBrush CreateMaskedBackdropBrush(this Windows.UI.Composition.Compositor compositor, CompositionProToolkit.ICompositionMask mask, Windows.UI.Color blendColor, float blurAmount, Windows.UI.Composition.CompositionBackdropBrush backdropBrush=null) { return default(Windows.UI.Composition.CompositionEffectBrush); }
+    }
     public partial interface ICompositionMask : System.IDisposable
     {
         CompositionProToolkit.ICompositionMaskGenerator Generator { get; }
---
+++ CompositionProToolkit/CompositorExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Windows.UI;
+using Windows.UI.Composition;
+using CompositionProToolkit;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+
+namespace CompositionProToolkit
+{
+    /// <summary>
+    /// Extension Methods for Compositor
+    /// </summary>
+    public static class CompositorExtensions
+    {
+        /// <summary>
+        /// Creates a custom shaped Effect Brush using BackdropBrush and a Mask
+        /// </summary>
+        /// <param name="compositor">Compositor</param>
+        /// <param name="mask">ICompositionMask</param>
+        /// <param name="blendColor">Color to blend in the BackdropBrush</param>
+        /// <param name="blurAmount">Blur Amount of the Backdrop Brush</param>
+        /// <param name="backdropBrush">Backdrop Brush (optional). If not provided, then compositor creates it.</param>
+        /// <returns>CompositionEffectBrush</returns>
+        public static CompositionEffectBrush CreateMaskedBackdropBrush(this Compositor compositor, ICompositionMask mask,
+            Color blendColor, float blurAmount, CompositionBackdropBrush backdropBrush = null)
+        {
+            // Blur Effect
+            var blurEffect = new GaussianBlurEffect()
+            {
+                Name = "Blur",
+                BlurAmount = blurAmount,
+                BorderMode = EffectBorderMode.Hard,
+                Optimization = EffectOptimization.Balanced,
+                Source = new CompositionEffectSourceParameter("backdrop"),
+            };
+
+            // Blend Effect
+            var blendEffect = new BlendEffect
+            {
+                Foreground = new ColorSourceEffect
+                {
+                    Name = "Color",
+                    Color = blendColor
+                },
+                Background = blurEffect,
+                Mode = BlendEffectMode.Multiply
+            };
+
+            // Composite Effect
+            var effect = new CompositeEffect
+            {
+                Mode = CanvasComposite.DestinationIn,
+                Sources =
+                {
+                    blendEffect,
+                    new CompositionEffectSourceParameter("mask")
+                }
+
+            };
+
+            // Create Effect Factory
+            var factory = compositor.CreateEffectFactory(effect, new[] { "Blur.BlurAmount", "Color.Color" });
+            // Create Effect Brush
+            var brush = factory.CreateBrush();
+
+            // Set the BackDropBrush
+            // If no backdrop brush is provided, create one
+            brush.SetSourceParameter("backdrop", backdropBrush ?? compositor.CreateBackdropBrush());
+
+            // Set the Mask
+            // Create SurfaceBrush from CompositionMask
+            var maskBrush = compositor.CreateSurfaceBrush(mask.Surface);
+            brush.SetSourceParameter("mask", maskBrush);
+
+            return brush;
+
+        }
+    }
+}
