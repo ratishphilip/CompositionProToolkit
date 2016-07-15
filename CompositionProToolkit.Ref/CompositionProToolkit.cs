2c177491778d135a158ef6387fdb509acc06d7ee
Ratish Philip <ratishphilip@gmail.com>
2016-07-15 15:27:14 -0700

    updated to v0.3
    Added FluidWrapPanel control

--- CompositionProToolkit.Ref/CompositionProToolkit.cs
+++ CompositionProToolkit.Ref/CompositionProToolkit.cs
@@ -28,8 +28,51 @@ namespace CompositionProToolkit
         System.Threading.Tasks.Task<CompositionProToolkit.ICompositionMask> CreateMaskAsync(Windows.Foundation.Size size, Microsoft.Graphics.Canvas.Geometry.CanvasGeometry geometry, Windows.UI.Color color);
     }
 }
+namespace CompositionProToolkit.Common
+{
+    public static partial class Utils
+    {
+        public static Windows.Foundation.Size CollapseThickness(this Windows.UI.Xaml.Thickness thick) { return default(Windows.Foundation.Size); }
+        public static Windows.Foundation.Rect Deflate(this Windows.Foundation.Rect rect, Windows.UI.Xaml.Thickness thick) { return default(Windows.Foundation.Rect); }
+        public static Windows.Foundation.Rect Inflate(this Windows.Foundation.Rect rect, Windows.UI.Xaml.Thickness thick) { return default(Windows.Foundation.Rect); }
+        public static bool IsCloseTo(this double value1, double value2) { return default(bool); }
+        public static bool IsCloseTo(this Windows.Foundation.Point point1, Windows.Foundation.Point point2) { return default(bool); }
+        public static bool IsCloseTo(this Windows.Foundation.Rect rect1, Windows.Foundation.Rect rect2) { return default(bool); }
+        public static bool IsCloseTo(this Windows.Foundation.Size size1, Windows.Foundation.Size size2) { return default(bool); }
+        public static bool IsEqualTo(this Windows.UI.Xaml.Media.Brush brush, Windows.UI.Xaml.Media.Brush otherBrush) { return default(bool); }
+        public static bool IsGreaterThan(this double value1, double value2) { return default(bool); }
+        public static bool IsLessThan(double value1, double value2) { return default(bool); }
+        public static bool IsNaN(double value) { return default(bool); }
+        public static bool IsOne(this double value) { return default(bool); }
+        public static bool IsOpaqueSolidColorBrush(this Windows.UI.Xaml.Media.Brush brush) { return default(bool); }
+        public static bool IsUniform(this Windows.UI.Xaml.CornerRadius corner) { return default(bool); }
+        public static bool IsUniform(this Windows.UI.Xaml.Thickness thick) { return default(bool); }
+        public static bool IsValid(this Windows.UI.Xaml.CornerRadius corner, bool allowNegative, bool allowNaN, bool allowPositiveInfinity, bool allowNegativeInfinity) { return default(bool); }
+        public static bool IsValid(this Windows.UI.Xaml.Thickness thick, bool allowNegative, bool allowNaN, bool allowPositiveInfinity, bool allowNegativeInfinity) { return default(bool); }
+        public static bool IsZero(this double value) { return default(bool); }
+        public static bool IsZero(this Windows.UI.Xaml.CornerRadius corner) { return default(bool); }
+        public static bool IsZero(this Windows.UI.Xaml.Thickness thick) { return default(bool); }
+        public static double RoundLayoutValue(double value, double dpiScale) { return default(double); }
+    }
+}
 namespace CompositionProToolkit.Controls
 {
+    public partial class FluidPointerDragBehavior : Microsoft.Xaml.Interactivity.Behavior<Windows.UI.Xaml.UIElement>
+    {
+        public static readonly Windows.UI.Xaml.DependencyProperty DragButtonProperty;
+        public FluidPointerDragBehavior() { }
+        public CompositionProToolkit.Controls.FluidPointerDragBehavior.DragButtonType DragButton { get { return default(CompositionProToolkit.Controls.FluidPointerDragBehavior.DragButtonType); } set { } }
+        protected override void OnAttached() { }
+        protected override void OnDetaching() { }
+        public enum DragButtonType
+        {
+            MouseLeftButton = 0,
+            MouseMiddleButton = 1,
+            MouseRightButton = 2,
+            Pen = 3,
+            Touch = 4,
+        }
+    }
     public partial class FluidProgressRing : Windows.UI.Xaml.Controls.Control
     {
         public static readonly Windows.UI.Xaml.DependencyProperty ActiveNodesProperty;
@@ -46,5 +89,44 @@ namespace CompositionProToolkit.Controls
         public double NodeSizeFactor { get { return default(double); } set { } }
         public System.TimeSpan RingDuration { get { return default(System.TimeSpan); } set { } }
         protected override Windows.Foundation.Size ArrangeOverride(Windows.Foundation.Size finalSize) { return default(Windows.Foundation.Size); }
+    }
+    public sealed partial class FluidWrapPanel : Windows.UI.Xaml.Controls.Panel
+    {
+        public static System.TimeSpan DefaultFluidAnimationDuration;
+        public const double DefaultItemHeight = 10;
+        public const double DefaultItemWidth = 10;
+        public static System.TimeSpan DefaultOpacityAnimationDuration;
+        public static System.TimeSpan DefaultScaleAnimationDuration;
+        public const double DragOpacityDefault = 0.7;
+        public static readonly Windows.UI.Xaml.DependencyProperty DragOpacityProperty;
+        public const double DragScaleDefault = 1.2;
+        public static readonly Windows.UI.Xaml.DependencyProperty DragScaleProperty;
+        public static readonly Windows.UI.Xaml.DependencyProperty FluidItemsProperty;
+        public static System.TimeSpan InitializationAnimationDuration;
+        public static readonly Windows.UI.Xaml.DependencyProperty IsComposingProperty;
+        public static readonly Windows.UI.Xaml.DependencyProperty ItemHeightProperty;
+        public static readonly Windows.UI.Xaml.DependencyProperty ItemsSourceProperty;
+        public static readonly Windows.UI.Xaml.DependencyProperty ItemWidthProperty;
+        public const double NormalOpacity = 1;
+        public const double NormalScale = 1;
+        public const double OpacityMin = 0.1;
+        public static readonly Windows.UI.Xaml.DependencyProperty OptimizeChildPlacementProperty;
+        public static readonly Windows.UI.Xaml.DependencyProperty OrientationProperty;
+        public const int ZIndexDrag = 10;
+        public const int ZIndexIntermediate = 1;
+        public const int ZIndexNormal = 0;
+        public FluidWrapPanel() { }
+        public double DragOpacity { get { return default(double); } set { } }
+        public double DragScale { get { return default(double); } set { } }
+        public System.Collections.ObjectModel.ObservableCollection<Windows.UI.Xaml.UIElement> FluidItems { get { return default(System.Collections.ObjectModel.ObservableCollection<Windows.UI.Xaml.UIElement>); } }
+        public bool IsComposing { get { return default(bool); } set { } }
+        public double ItemHeight { get { return default(double); } set { } }
+        public System.Collections.IEnumerable ItemsSource { get { return default(System.Collections.IEnumerable); } set { } }
+        public double ItemWidth { get { return default(double); } set { } }
+        public bool OptimizeChildPlacement { get { return default(bool); } set { } }
+        public Windows.UI.Xaml.Controls.Orientation Orientation { get { return default(Windows.UI.Xaml.Controls.Orientation); } set { } }
+        public System.Threading.Tasks.Task AddChildAsync(Windows.UI.Xaml.FrameworkElement child) { return default(System.Threading.Tasks.Task); }
+        protected override Windows.Foundation.Size ArrangeOverride(Windows.Foundation.Size finalSize) { return default(Windows.Foundation.Size); }
+        protected override Windows.Foundation.Size MeasureOverride(Windows.Foundation.Size availableSize) { return default(Windows.Foundation.Size); }
     }
 }
