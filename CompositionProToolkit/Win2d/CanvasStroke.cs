8b7e58400776eb0bab28a01906b6a7e0b07fbad0
Ratish Philip <ratishphilip@gmail.com>
2017-01-31 21:52:29 -0800

    updated to v0.5.1
    Updated version info in file headers.

--- CompositionProToolkit/Win2d/CanvasStroke.cs
+++ CompositionProToolkit/Win2d/CanvasStroke.cs
@@ -24,7 +24,7 @@
 // This file is part of the CompositionProToolkit project: 
 // https://github.com/ratishphilip/CompositionProToolkit
 //
-// CompositionProToolkit v0.5.0
+// CompositionProToolkit v0.5.1
 // 
 
 using System.Numerics;
